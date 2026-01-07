ea4cb5823f7a9ae15945c8589c41ce06e3ca49aa
Alex <st20328728@outlook.cardiffmet.ac.uk>
2026-01-07 00:39:03 +0000

    Added player movement

---
+++ gdv4002-base1/gdv4002-base1/Player.h
@@ -0,0 +1,17 @@
+#include "GameObject2D.h"
+
+class Player : public GameObject2D {
+
+private:
+
+	float playerSpeed;
+
+
+
+public:
+
+	Player(glm::vec2 initPosition, float initOrientation, glm::vec2 initSize, GLuint initTextureID, float initialPlayerSpeed);
+	void update(double tDelta) override;
+
+
+};
--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,8 +1,10 @@
 #include "Engine.h"
+#include "Keys.h"
+#include <bitset>
 
-
+std::bitset<5> keys{ 0x0 };
 // Function prototypes
-
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 // Global Variables
 
@@ -27,7 +29,7 @@ int main(void) {
 	//
 	// Set callback functions
 	//
-	
+	setKeyboardHandler(myKeyboardHandler);
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
@@ -35,6 +37,64 @@ int main(void) {
 	// When we quit (close window for example), clean up engine resources
 	engineShutdown();
 
+
 	// return success :)
 	return 0;
+}
+
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	// Check if the key was just pressed
+	if (action == GLFW_PRESS) {
+
+		// now check which key was pressed...
+		switch (key)
+		{
+		case GLFW_KEY_ESCAPE:
+			// If escape is pressed tell GLFW we want to close the window (and quit)
+		
+				glfwSetWindowShouldClose(window, true);
+			break;
+
+		case GLFW_KEY_W:
+			printf("w pressed\n");
+			break;
+
+		case GLFW_KEY_A:
+			printf("a pressed\n");
+			break;
+
+		case GLFW_KEY_S:
+			printf("s pressed\n");
+			break;
+
+		case GLFW_KEY_D:
+			printf("d  pressed\n");
+			break;
+		}
+	}
+	// If not pressed, check the key has just been released
+	else if (action == GLFW_RELEASE) {
+
+		// handle key release events
+		switch (key)
+		{
+		case GLFW_KEY_W:
+			printf("w released\n");
+			break;
+
+		case GLFW_KEY_A:
+			printf("a released\n");
+			break;
+
+		case GLFW_KEY_S:
+			printf("s released\n");
+			break;
+
+		case GLFW_KEY_D:
+			printf("d released\n");
+			break;
+		}
+
+	}
 }
